d416f20c8210d312ae72a6488ac552620163ba7d
jakjakwongwong <mr.jigdaw@gmail.com>
2013-12-03 17:44:48 +0700

    Initial Commit

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by Jigdaw on 12/3/13.
+//  Copyright (c) 2013 arnan_srisontisuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testGit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by Jigdaw on 12/3/13.
+//  Copyright (c) 2013 arnan_srisontisuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
